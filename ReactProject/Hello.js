4301b8a6de323c0b999b1bb0909072475e2df4ec
N P <nathaniel_pather@hotmail.com>
2019-09-06 22:09:03 +1000

    Transpiled JSX to Js. Solved static react component bug.

--- ReactProject/Hello.js
+++ ReactProject/Hello.js
@@ -1,7 +1,17 @@
-﻿const name = 'Josh Perez';
+﻿/*
+JSX code.
+const name = 'Josh Perez';
 const element = `<h1>Hello, ${name}</h1>`;
 
 ReactDOM.render(
     element,
     document.getElementById('root')
 );
+*/
+
+// Js alternative. Should be transpiled from JSX, however difficult to setup.
+"use strict";
+
+var name = 'Nathaniel Pather';
+var element = React.createElement("h1", null, "Hello, ", name);
+ReactDOM.render(element, document.getElementById('hello_container'));
